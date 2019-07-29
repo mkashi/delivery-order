fd4123db53d84440c00816235a28ceb368692d89
Mourad <mourad.k@gmail.com>
2019-07-29 17:24:33 +0200

    add link department for slug

---
+++ Controller/DepartmentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Delivery\OrderBundle\Controller;
+
+use Delivery\ApiBundle\Entity\Localisation\Department;
+use Delivery\ApiBundle\Manager\CategoryManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DepartmentController
+ */
+class DepartmentController extends Controller
+{
+    /**
+     * @ParamConverter("department", options={"mapping": {"slug": "slug"}})
+     *
+     * @Route("/restaurant-livraison/commander-en-ligne/{slug}/department", name="department_order")
+     *
+     * @param Department      $department
+     * @param CategoryManager $categoryManager
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function departmentAction(Department $department, CategoryManager $categoryManager)
+    {
+        $categories = $categoryManager->getPublishedCategoriesAndProducts();
+
+        return $this->render('@DeliveryOrder/department/order.html.twig', [
+            'categories' => $categories,
+            'department' => $department,
+        ]);
+    }
+}
